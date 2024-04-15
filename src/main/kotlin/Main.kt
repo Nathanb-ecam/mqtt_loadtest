6a2b3e4b0c8d32147e98b8eeacfb33c660d9d68a
Nathanb-ecam <18092@ecam.be>
2024-04-15 15:06:49 +0200

    cleaned comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -90,42 +90,6 @@ fun main() {
             }
         }
     }.start(wait = true)
-
-    /*val messagePayload = "Hello, MQTT!"*/
-
-
-/*    val loadConfig = LoadConfig(
-        qos = MqttQoS.AT_MOST_ONCE,
-        *//*keepAliveSec = 60,*//*
-        messagePayloadSize = 500,
-        nMessagesPerChannel = 1000,
-        channelsPerThread = 100, // the amount of mqtt client connections by thread
-        *//*eventLoopsPerGroup = 1, // the amount of thread*//*
-        amountOfGroups = 10
-    )*/
-/*    val loadConfig = LoadConfig(
-    qos = MqttQoS.AT_MOST_ONCE,
-        *//*keepAliveSec = 60,*//*
-        messagePayloadSize = 500,
-        nMessagesPerChannel = 1000,
-        channelsPerThread = 100, // the amount of mqtt client connections by thread
-        *//*eventLoopsPerGroup = 1, // the amount of thread*//*
-        amountOfGroups = 10
-    )
-
-    val loadTest = LoadTester(
-        *//*broker = "localhost",*//*
-
-        port = 1883,
-        topic = "test",
-        loadConfig
-    )
-
-    val info = MessageInfoMetrics(loadConfig)
-    info.printParameters()
-    loadTest.launch()*/
-
-
 }
 
 
